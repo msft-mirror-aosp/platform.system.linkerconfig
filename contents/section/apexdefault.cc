d5620a07b8cc83c2d8ff0d0fc513273f5bebb082
Jooyung Han <jooyung@google.com>
2023-10-12 15:19:46 +0900

    Add :mediacas alias
    which exports "mediacas" namespace for vendor apex which needs to open
    plugsin from /vendor/lib/mediacas.
    
    Bug: 298542407
    Test: linkerconfig_diff_test
    Change-Id: I50d9dfc08743caef0ed5273556dc912505581d1a
    (cherry picked from commit 6750e7a4314199fd7a6b547f69f6ece65f853434)

--- contents/section/apexdefault.cc
+++ contents/section/apexdefault.cc
@@ -33,6 +33,7 @@ using android::linkerconfig::modules::LibProviders;
 using android::linkerconfig::modules::Namespace;
 using android::linkerconfig::modules::SharedLibs;
 using android::linkerconfig::modules::Section;
+using android::linkerconfig::modules::SharedLibs;
 
 namespace android {
 namespace linkerconfig {
@@ -145,6 +146,10 @@ Section BuildApexDefaultSection(Context& ctx, const ApexInfo& apex_info) {
         std::bind(BuildVndkNamespace, ctx, user_partition),
         SharedLibs{{Var("VNDK_SAMEPROCESS_LIBRARIES_" + user_partition_suffix)}},
     }};
+  }
+
+  if (apex_info.InVendor()) {
+    AddVendorSubdirNamespaceProviders(ctx, libs_providers);
   }
 
   return BuildSection(
